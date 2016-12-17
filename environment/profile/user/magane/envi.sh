44d77309833b4b3cb855d38fd1d89ff97ec0a509
TonyMeteo <tonytam874@gmail.com>
2016-12-16 20:41:35 -0500

    i just finish my first commit, i had to install multi arch headers to shut error messages up(lol)

---
+++ environment/profile/user/magane/envi.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export KANETON_USER="magane" && echo $KANETON_USER &&
+export KANETON_HOST="linux/x86_64" && echo $KANETON_HOST &&
+export KANETON_PLATFORM="ibm-pc" && echo $KANETON_PLATFORM &&
+export KANETON_ARCHITECTURE="ia32/educational" && echo $KANETON_ARCHITECTURE &&
+export KANETON_PYTHON="/usr/bin/python" && echo $KANETON_PYTHON
